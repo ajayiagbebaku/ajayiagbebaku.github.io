a3ca250e7a361c4a3e16437631615700938f9962
Ajayi Agbebaku Jr <ajayi1987@me.com>
2021-10-08 11:47:22 -0500

    Finished game and updated portfolio

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -12,28 +12,52 @@ function getComputerChoice() {
     const choices = ['r', 'p', 's'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-
 }
 
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + " You win! ";
+}
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " loses " + convertToWord(computerChoice) + " You lose! Loser!! ";
+}
+
+function draw(userChoice, computerChoice) {
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = convertToWord(userChoice) + " equals " + convertToWord(computerChoice) + " It's a draw! ";
+}
+
+ 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice){
         case "rs":
         case "pr":
         case "sp":
-            console.log("USER WINS.");
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            console.log("USER LOSES.");
+            lose();
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("It's a draw.");
+            draw();
             break;
-            
+
     }
     
 }
